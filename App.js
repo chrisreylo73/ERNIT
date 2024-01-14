4e0708d9a9ac3a28f108e622047d1aec1793cf59
chrisreylo73 <chrisreylo73@gmail.com>
2024-01-13 22:38:36 -0700

    Modal is now in AddMenu component

--- App.js
+++ App.js
@@ -1,191 +1,40 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity, FlatList, Modal, TextInput } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useState, useEffect } from "react";
-import ErnitModule from "./components/ErnitModule";
-import ImagePickerComponent from "./components/ImagePickerComponent";
-import { AntDesign } from "@expo/vector-icons";
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Modal, TextInput } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { BlurView } from "expo-blur";
+import ErnitModule from "./components/ErnitModule";
+import AddMenu from "./components/AddMenu";
+
 export default function App() {
 	const [isModalVisible, setModalVisible] = useState(false);
-	const [titleInput, setTitleInput] = useState("");
-	const [totalDaysInput, setTotalDaysInput] = useState("");
-	const [rewardLinkInput, setRewardLinkInput] = useState("");
-	const [rewardImage, setRewardImage] = useState(null);
-	const [data, setData] = useState([
-		//accentColor: "rgba(170, 216, 92, 0.6)}"
-		//accentColor: "rgba(255, 0, 0, 0.6)"
-		//accentColor: "rgba(128, 0, 128, 0.6)"
-		//accentColor: "rgba(150, 216, 250, 0.6)"
-		// { id: "1", totalDays: 36, title: "Practice Guitar", image: require("./assets/Forte-Port-Nylon-HO.png"), link: "https://www.michaelkellyguitars.com/en/products/view/forte-port-nylon" },
-		// { id: "2", totalDays: 25, title: "Play Basketball", image: require("./assets/1.jpg"), link: "https://www.flightclub.com/air-jordan-12-retro-cool-grey-white-team-orange-011569" },
-		// { id: "3", totalDays: 16, title: "Leet Code", image: require("./assets/nirvana.jpg"), link: "https://example.com/module1" },
-		// { id: "4", totalDays: 9, title: "Make an APP", image: require("./assets/2.jpg"), link: "https://www.amazon.com/dp/B06XNX6QJ4/?coliid=I34XSE63MAY7VL&colid=299X7CKVGCA6D&psc=0&ref_=list_c_wl_lv_ov_lig_dp_it" },
-		// { id: "5", totalDays: 4, title: "Workout", image: require("./assets/3.jpg"), link: "https://powerblock.com/product/commercial-pro-175-lb-adjustable-dumbbell/" },
-	]);
-
-	const handleCreateButtonPress = async () => {
-		console.log("Data:", data);
-		// Validate input fields before adding a new module
-		if (!titleInput || !totalDaysInput || !rewardLinkInput || !rewardImage) {
-			// Handle validation error (you can display an alert or do something else)
-			console.log("TitleInput: ", titleInput);
-			console.log("TotalDaysInput: ", totalDaysInput);
-			console.log("RewardLinkInput: ", rewardLinkInput);
-			console.log("RewardImage: ", rewardImage);
-			return;
-		}
-		// Add a new module to the data array
-		const newModule = {
-			id: String(data.length + 1),
-			totalDays: parseInt(totalDaysInput),
-			title: titleInput,
-			image: rewardImage,
-			link: rewardLinkInput,
-		};
-		// Update the data state with the new module
-		setData((prevData) => [...prevData, newModule]);
-
-		try {
-			// Save the new data to AsyncStorage
-			await AsyncStorage.setItem("modules", JSON.stringify([...data, newModule]));
-			// Clear input fields and set rewardImage to null after successful save
-			setTitleInput("");
-			setTotalDaysInput("");
-			setRewardLinkInput("");
-			setRewardImage(nusl);
-			// Close the modal
-			setModalVisible(false);
-		} catch (error) {
-			console.error("Error saving data:", error);
-		}
-	};
-
-	const loadData = async () => {
-		try {
-			const storedData = await AsyncStorage.getItem("modules");
-			if (storedData) {
-				setData(JSON.parse(storedData));
-			}
-		} catch (error) {
-			console.error("Error loading data:", error);
-		}
-	};
-
-	useEffect(() => {
-		loadData();
-	}, []);
+	const [data, setData] = useState([]);
 
 	return (
 		<View style={styles.container}>
 			<StatusBar style="auto" />
 			<Header />
-			<View style={styles.padding}></View>
 			<View style={styles.list}>
 				<FlatList contentContainerStyle={{ paddingBottom: 60, paddingTop: 85 }} showsVerticalScrollIndicator={false} data={data} keyExtractor={(item) => item.id} renderItem={({ item }) => <ErnitModule totalDays={item.totalDays} title={item.title} image={item.image} link={item.link} accentColor={item.accentColor} />} />
 			</View>
-			<View style={styles.padding}></View>
 			<Footer setModalVisible={setModalVisible} />
-			<Modal useNativeDriver={true} animationType="slide" transparent={true} visible={isModalVisible} onRequestClose={() => setModalVisible(false)}>
-				<View intensity={100} tint="dark" style={styles.modalContainer}>
-					{/* <Text style={styles.modalText}>Create New </Text> */}
-					<View style={styles.modalContainerRow}>
-						<Text style={styles.modalText}>Action Title:</Text>
-						<TextInput placeholder="Enter daily action title" value={titleInput} onChangeText={(text) => setTitleInput(text)} style={styles.modalInput} />
-					</View>
-					<View style={styles.modalContainerRow}>
-						<Text style={styles.modalText}>Total Days:</Text>
-						<TextInput placeholder="Enter number of days" keyboardType="numeric" style={styles.modalInput} value={totalDaysInput} onChangeText={(text) => setTotalDaysInput(text)} />
-					</View>
-					<View style={styles.modalContainerRow}>
-						<Text style={styles.modalText}>Reward Link:</Text>
-						<TextInput placeholder="http://" value={rewardLinkInput} onChangeText={(text) => setRewardLinkInput(text)} style={styles.modalInput} />
-					</View>
-					<View style={styles.modalContainerRow}>
-						<Text style={styles.modalText}>Reward Image:</Text>
-						<ImagePickerComponent setRewardImage={setRewardImage} rewardImage={rewardImage} />
-					</View>
-					<TouchableOpacity style={styles.createButton} onPress={handleCreateButtonPress}>
-						<AntDesign name="plus" size={24} color="white" />
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.closeButton} onPress={() => setModalVisible(false)}>
-						<AntDesign name="close" size={24} color="white" />
-					</TouchableOpacity>
-				</View>
-			</Modal>
+			<AddMenu data={data} setData={setData} isModalVisible={isModalVisible} setModalVisible={setModalVisible} />
 		</View>
 	);
 }
-
-//rgba(144, 238, 144, 0.7)
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		//backgroundColor: "black",
 		backgroundColor: "#111111",
-		//backgroundColor: "#2d3460",
 		alignItems: "center",
-		justifyContent: "center", // Center content vertically
+		justifyContent: "center",
 	},
 	list: {
-		// flex: 1,
-		width: "100%", // Adjust width as needed
+		width: "100%",
 		justifyContent: "center",
 		alignItems: "center",
-		// marginVertical: 60,
-		// Adjust height as needed
 		marginTop: 0,
 		marginBottom: 0,
-		// overflow: "hidden",
-	},
-	button: {
-		alignItems: "center",
-		justifyContent: "center",
-		// backgroundColor: "black",
-		padding: 12,
-		borderRadius: 20,
-		// marginBottom: 100,
-		//borderWidth: 1,
-	},
-	modalContainer: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-		backgroundColor: "#111111",
-		width: "100%",
-		hight: "100%",
-	},
-	modalContainerRow: {
-		padding: 10,
-		flexDirection: "row",
-		alignItems: "center",
-		justifyContent: "space-between",
-		width: "90%",
-	},
-	modalText: {
-		color: "white",
-		fontSize: 18,
-		marginRight: 10,
-		// marginBottom: 20,
-	},
-	modalInput: {
-		backgroundColor: "white",
-		width: 200,
-		borderRadius: 5,
-		padding: 3,
-	},
-	createButton: {
-		alignItems: "center",
-		justifyContent: "center",
-		// backgroundColor: "black",
-		padding: 16,
-		borderRadius: 20,
-	},
-	closeButton: {
-		position: "absolute",
-		bottom: 25,
 	},
 });
 
@@ -195,3 +44,7 @@ const styles = StyleSheet.create({
 // } catch (error) {
 // 	console.error("Error clearing AsyncStorage:", error);
 // }
+//accentColor: "rgba(170, 216, 92, 0.6)}"
+//accentColor: "rgba(255, 0, 0, 0.6)"
+//accentColor: "rgba(128, 0, 128, 0.6)"
+//accentColor: "rgba(150, 216, 250, 0.6)"
--- components/AddMenu.js
+++ components/AddMenu.js
@@ -1,26 +1,82 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Modal, TextInput } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AntDesign } from "@expo/vector-icons";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import ErnitModule from "./components/ErnitModule";
+import ImagePickerComponent from "./ImagePickerComponent";
+// import AddMenu from "./components/AddMenu";
 
-const AddMenu = () => {
+const AddMenu = ({ setModalVisible, isModalVisible, data, setData }) => {
+	const [ernitModuleTitle, setErnitModuleTitle] = useState("");
+	const [totalDays, setTotalDays] = useState("");
+	const [rewardLink, setRewardLink] = useState("");
+	const [rewardImage, setRewardImage] = useState(null);
+	// const [data, setData] = useState([]);
+
+	const handleCreateButtonPress = async () => {
+		console.log("Data:", data);
+		if (!ernitModuleTitle || !totalDays || !rewardLink || !rewardImage) {
+			console.log("ernitModuleTitle: ", ernitModuleTitle);
+			console.log("totalDays: ", totalDays);
+			console.log("RewardLink: ", rewardLink);
+			console.log("RewardImage: ", rewardImage);
+			return;
+		}
+		const newModule = {
+			id: String(data.length + 1),
+			totalDays: parseInt(totalDays),
+			title: ernitModuleTitle,
+			image: rewardImage,
+			link: rewardLink,
+		};
+		setData((prevData) => [...prevData, newModule]);
+		try {
+			await AsyncStorage.setItem("modules", JSON.stringify([...data, newModule]));
+			setErnitModuleTitle("");
+			setTotalDays("");
+			setRewardLink("");
+			setRewardImage(null);
+			setModalVisible(false);
+		} catch (error) {
+			console.error("Error saving data:", error);
+		}
+	};
+
+	useEffect(() => {
+		loadData();
+	}, []);
+
+	const loadData = async () => {
+		try {
+			const storedData = await AsyncStorage.getItem("modules");
+			if (storedData) {
+				setData(JSON.parse(storedData));
+			}
+		} catch (error) {
+			console.error("Error loading data:", error);
+		}
+	};
 	return (
 		<Modal animationType="fade" transparent={true} visible={isModalVisible} onRequestClose={() => setModalVisible(false)}>
 			<View style={styles.modalContainer}>
-				{/* <Text style={styles.modalText}>Create New </Text> */}
 				<View style={styles.modalContainerRow}>
 					<Text style={styles.modalText}>Action Title:</Text>
-					<TextInput placeholder="Enter daily action title" value={titleInput} onChangeText={(text) => setTitleInput(text)} style={styles.modalInput} />
+					<TextInput placeholder="Enter daily action title" value={ernitModuleTitle} onChangeText={(text) => setErnitModuleTitle(text)} style={styles.modalInput} />
 				</View>
 				<View style={styles.modalContainerRow}>
 					<Text style={styles.modalText}>Total Days:</Text>
-					<TextInput placeholder="Enter number of days" keyboardType="numeric" style={styles.modalInput} value={totalDaysInput} onChangeText={(text) => setTotalDaysInput(text)} />
+					<TextInput placeholder="Enter number of days" keyboardType="numeric" style={styles.modalInput} value={totalDays} onChangeText={(text) => setTotalDays(text)} />
 				</View>
 				<View style={styles.modalContainerRow}>
 					<Text style={styles.modalText}>Reward Link:</Text>
-					<TextInput placeholder="http://" value={rewardLinkInput} onChangeText={(text) => setRewardLinkInput(text)} style={styles.modalInput} />
+					<TextInput placeholder="http://" value={rewardLink} onChangeText={(text) => setRewardLink(text)} style={styles.modalInput} />
 				</View>
 				<View style={styles.modalContainerRow}>
 					<Text style={styles.modalText}>Reward Image:</Text>
-					<ImagePickerComponent />
+					<ImagePickerComponent setRewardImage={setRewardImage} rewardImage={rewardImage} />
 				</View>
 				<TouchableOpacity style={styles.createButton} onPress={handleCreateButtonPress}>
 					<AntDesign name="plus" size={24} color="white" />
@@ -40,7 +96,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center",
-		backgroundColor: "rgba(0, 0, 0, .92)",
+		backgroundColor: "#111111",
 		width: "100%",
 		hight: "100%",
 	},
@@ -55,7 +111,6 @@ const styles = StyleSheet.create({
 		color: "white",
 		fontSize: 18,
 		marginRight: 10,
-		// marginBottom: 20,
 	},
 	modalInput: {
 		backgroundColor: "white",
@@ -66,7 +121,6 @@ const styles = StyleSheet.create({
 	createButton: {
 		alignItems: "center",
 		justifyContent: "center",
-		// backgroundColor: "black",
 		padding: 16,
 		borderRadius: 20,
 	},
